44df66372a766bc5120005592a065ede9c0f73a6
KaranSapra124 <sapra7112@gmail.com>
2025-04-27 01:22:21 +0530

    Added FOrecast for several days

--- app/Components/Weather_Components/ExtraDetails.tsx
+++ app/Components/Weather_Components/ExtraDetails.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { Feather } from '@expo/vector-icons';
+import { Feather, FontAwesome6 } from '@expo/vector-icons';
 
 
 export interface details {
@@ -17,7 +17,16 @@ const ExtraDetails: React.FC<{ details: details | null }> = ({ details }) => {
                     <Feather name='sun' size={16} color="#2563EB" iconStyle='solid' />
                     <Text>UV</Text>
                     <Text>{details?.uv}</Text>
-
+                </View>
+                <View>
+                    <FontAwesome6 name='temperature-high' size={16} color="#2563EB" iconStyle='solid' />
+                    <Text>Feels Like</Text>
+                    <Text>{details?.feelsLike}</Text>
+                </View>
+                <View>
+                    <Feather name='droplet' size={16} color="#2563EB" iconStyle='solid' />
+                    <Text>Humidity</Text>
+                    <Text>{details?.humidity}</Text>
                 </View>
             </View>
         </>
