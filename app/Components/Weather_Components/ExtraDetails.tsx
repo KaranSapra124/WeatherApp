a29ab640e81dc88e63d211b46cdcdc496da1f619
KaranSapra124 <sapra7112@gmail.com>
2025-04-27 01:43:28 +0530

    Added Extra Details

--- app/Components/Weather_Components/ExtraDetails.tsx
+++ app/Components/Weather_Components/ExtraDetails.tsx
@@ -9,24 +9,24 @@ export interface details {
     humidity: number
 }
 const ExtraDetails: React.FC<{ details: details | null }> = ({ details }) => {
-    console.log(details)
+    // console.log(details)
     return (
         <>
-            <View>
-                <View>
+            <View className='flex flex-row my-2 justify-between items-center'>
+                <View className='shadow-lg bg-white/50 p-2 rounded w-24 h-22'>
                     <Feather name='sun' size={16} color="#2563EB" iconStyle='solid' />
-                    <Text>UV</Text>
-                    <Text>{details?.uv}</Text>
+                    <Text className='text-blue-900 font-bold text-md my-1'>UV</Text>
+                    <Text className='text-blue-900 font-semibold '>{details?.uv}</Text>
                 </View>
-                <View>
+                <View className='shadow-lg bg-white/50 p-2 rounded w-24 h-22'>
                     <FontAwesome6 name='temperature-high' size={16} color="#2563EB" iconStyle='solid' />
-                    <Text>Feels Like</Text>
-                    <Text>{details?.feelsLike}</Text>
+                    <Text className='text-blue-900 font-bold text-md my-1'>Feels Like</Text>
+                    <Text className='text-blue-900 font-semibold '>{details?.feelsLike}</Text>
                 </View>
-                <View>
+                <View className='shadow-lg bg-white/50 p-2 rounded w-24 h-22'>
                     <Feather name='droplet' size={16} color="#2563EB" iconStyle='solid' />
-                    <Text>Humidity</Text>
-                    <Text>{details?.humidity}</Text>
+                    <Text className='text-blue-900 font-bold text-md my-1'>Humidity</Text>
+                    <Text className='text-blue-900 font-semibold '>{details?.humidity}</Text>
                 </View>
             </View>
         </>
